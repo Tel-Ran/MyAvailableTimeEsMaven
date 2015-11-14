ede66bc163ac0335614a52a15502291af3657e80
gekster <boxfornews@yandex.ru>
2015-11-14 23:56:08 +0200

    Changing IExternalServises interface Initial adding ExternalServises class

--- src/main/java/com/mat/interfaces/IExternalServices.java
+++ src/main/java/com/mat/interfaces/IExternalServices.java
@@ -5,12 +5,7 @@ import com.mat.json.*;
 import java.util.List;
 
 public interface IExternalServices {
-    boolean upload(UploadRequest request) throws Throwable;
-    DownloadEventsResponse download(DownloadEventsRequest request) throws Throwable;
-    boolean authorize(User user);
-    List<Contact> getContacts(int userId, List<Scheduler> schedulers) throws Throwable;
-    //null if not authorised
-    List<ExternalCalendar> getCalendars(int userId, List<Scheduler> schedulers) throws Throwable; //was added at 03.11.2015
-    List<Scheduler> getAuthorizedSchedulers(int userId, List<Scheduler> schedulers) throws Throwable;
-    void setToken(Credential credential); //
+    Credential getCredential(int userId, Scheduler scheduler);
+    void setCredential(int userId, Scheduler scheduler, Credential credential);
+    
 }
---
+++ src/main/java/com/mat/model/ExternalServises.java
@@ -0,0 +1,25 @@
+package com.mat.model;
+
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.mat.interfaces.IExternalServices;
+import com.mat.json.Scheduler;
+
+
+public class ExternalServises implements IExternalServices{
+
+	public Credential getCredential(int userId, Scheduler scheduler) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setCredential(int userId, Scheduler scheduler, Credential credential) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+
+}
