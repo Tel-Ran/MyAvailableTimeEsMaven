88a811f3465fca03667b00f808729fed56d5144c
gekster <boxfornews@yandex.ru>
2015-11-15 13:33:12 +0200

    Deleting maps and wrong methods from EScontroller class

--- src/main/java/com/mat/model/EScontroller.java
+++ src/main/java/com/mat/model/EScontroller.java
@@ -9,29 +9,13 @@ import com.google.api.client.auth.oauth2.Credential;
 
 public class EScontroller implements IExternalServices {
 	
-	HashMap<Integer, HashMap<Scheduler, List<ExternalCalendar>>> calendars = new HashMap<Integer, HashMap<Scheduler,List<ExternalCalendar>>>();
-	HashMap<Integer, HashMap<Scheduler, List<Contact>>> contacts = new HashMap<Integer, HashMap<Scheduler,List<Contact>>>(); 
-	
 		public List<ExternalCalendar> getCalendars(int userId, List<Scheduler> schedulers) {
-			List<ExternalCalendar> exCalendars = new ArrayList<ExternalCalendar>();
-			for (int i = 0; i < schedulers.size(); i++) {
-				List<ExternalCalendar> tempCal = calendars.get(userId).get(schedulers.get(i));
-				if (tempCal != null)
-					exCalendars.addAll(tempCal);
-			}
-			return exCalendars;
-				
+			return null;
 			
 	}
 
 	public List<Contact> getContacts(int userId, List<Scheduler> schedulers) {
-		List<Contact> tmpContacts = new ArrayList<Contact>();
-		for (int i = 0; i < schedulers.size(); i++) {
-			List<Contact> tempList = contacts.get(userId).get(contacts.get(i));
-			if (tempList != null)
-				tmpContacts.addAll(tempList);
-		}
-		return tmpContacts;
+		return null;
 			
 		}
 
