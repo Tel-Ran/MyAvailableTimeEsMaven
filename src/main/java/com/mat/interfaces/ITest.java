534d02a77f8296f00e806ea45f8ff8047a30c3a8
gekster <boxfornews@yandex.ru>
2015-11-19 17:50:37 +0200

    fizing errors in ITest and TestController

--- src/main/java/com/mat/interfaces/ITest.java
+++ src/main/java/com/mat/interfaces/ITest.java
@@ -1,8 +1,8 @@
 package com.mat.interfaces;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.mat.json.Credential;
 import com.mat.json.Scheduler;
 
 public interface ITest {
-GoogleCredential getCredential(int userId, Scheduler scheduler);
+Credential getCredential(int userId, Scheduler scheduler);
 }
